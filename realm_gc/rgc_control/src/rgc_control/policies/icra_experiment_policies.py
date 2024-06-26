4172d6d7da9f40cdfdff1ef179fdb2b6e76773ec
Anjali <anjaliparashar12@gmail.com>
2024-06-26 13:40:33 -0400

    Adding LQR Trajectory tracking for F1tenth

--- realm_gc/rgc_control/src/rgc_control/policies/icra_experiment_policies.py
+++ realm_gc/rgc_control/src/rgc_control/policies/icra_experiment_policies.py
@@ -4,73 +4,21 @@ from dataclasses import dataclass
 import jax.numpy as jnp
 import numpy as np
 from rgc_control.policies.composite import CompositePolicy
-from rgc_control.policies.tracking.steering_policies import (
-    F1TenthSpeedSteeringPolicy, F1TenthSteeringPolicy
+from rgc_control.policies.tracking.steering_policies import ( F1TenthSteeringPolicy
 )
 from rgc_control.policies.tracking.tracking_policies import (
-    TimedG2CPose2DObservation,
-    G2CTrajectoryTrackingPolicy,
+    TimedPose2DObservation,
     TrajectoryTrackingPolicy,
 )
-from rgc_control.policies.tracking.trajectory import SplineTrajectory2D, LinearTrajectory2D
+from rgc_control.policies.tracking.trajectory import LinearTrajectory2D
 
+#Implements a trajectory-tracking LQR policy using an eqx filepath to give the waypoints via Bicycle dynamic model
 
 @dataclass
-class RALF1tenthObservation(TimedG2CPose2DObservation):
-    """The observation type for the F1 tenth ego agent in the RAL experiment."""
+class ICRAF1TenthObservation(TimedPose2DObservation):
+    """The observation type for the turtlebot nonego agents in the TRO experiment."""
 
-#Implements a path-tracking LQR policy, which currently does not work very well
-def create_ral_f1tenth_policy(
-    initial_position, v_ref, traj_csv_path
-) -> CompositePolicy:
-    """Create a composite policy for the F1tenth ego agent in the RAL experiment.
-
-    Args:
-        initial_position: The initial 2D position of F1 tenth.
-        traj_csv_path: The path to the trajectory (stored in an CSV file).
-    """
-    # Construct the components of the policy using the parameters they were trained with
-
-    # Load the trajectory and flip the x and y coordinates, then add some noise
-    ego_traj = SplineTrajectory2D(v_ref, traj_csv_path)
-
-    # Start pointing along +y in the highbay
-    desired_equilibrium_state = jnp.array([0.0, 0.0, jnp.pi / 2.0, 1.5])
-
-    #p = jnp.fliplr(ego_traj.p) 
-
-    # # Clamp the initial position to be the intended starting position
-    # if p[0, 1] <= -3.0:
-    #     p = p.at[0, 0].set(-0.5)
-    # else:
-    #     p = p.at[0, 0].set(0.5)
-
-    # Shift to +y to account for limited highbay space
-    #p = p.at[:, 1].add(0.5)
-
-    # Upscale if it's small
-    #if p.shape == (2, 2):
-    #    p_new = jnp.zeros((6, 2))
-    #    p_new = p_new.at[:, 0].set(jnp.interp(jnp.linspace(0, 1, 6), jnp.array([0.0, 1.0]), p[:, 0]))
-    #    p_new = p_new.at[:, 1].set(jnp.interp(jnp.linspace(0, 1, 6), jnp.array([0.0, 1.0]), p[:, 1]))
-    #    p = p_new
-
-    print("Loaded trajectory with waypoints:")
-
-    print(ego_traj)
-
-    # Make the trajectory tracking policy
-    steering_controller = F1TenthSpeedSteeringPolicy(
-        equilibrium_state=desired_equilibrium_state,
-        axle_length=0.28,
-        dt=0.1,
-    )
-    ego_tracking_policy = G2CTrajectoryTrackingPolicy(ego_traj, steering_controller)
-
-    return ego_tracking_policy
-
-#Implements a trajectory-tracking LQR policy using an eqx filepath to give the waypoints
-def create_lqr_f1tenth_policy(
+def create_icra_f1tenth_policy(
     initial_position, traj_eqx_path, randomize=False
 ) -> CompositePolicy:
     """Create a composite policy for the turtlebot nonego agents in the TRO experiment.
@@ -84,6 +32,7 @@ def create_lqr_f1tenth_policy(
 
     # Start pointing along +y in the highbay
     desired_equilibrium_state = jnp.array([0.0, 0.0, jnp.pi / 2.0, 1.5])
+    print("Instantiating LQR Tracking policy at [0,0,pi/2,1.5]")
 
     # Load the trajectory and flip x and y to convert from sim to high bay layout
     ego_traj = LinearTrajectory2D.from_eqx(6, traj_eqx_path)
@@ -109,12 +58,10 @@ if __name__ == "__main__":
     matplotlib.use("Agg")
 
     # Load the policies
-    #ego_state = jnp.array([-5.5, -0.5, 0.0, 2.0 * 0.5,0.0,0.0])
-    ego_state = jnp.array([-2.0, -0.0, 0.0, 2.0 * 0.5,0.0,0.0])
-    f1tenth_policy = create_ral_f1tenth_policy(
+    ego_state = jnp.array([-5.5, -0.5, 0.0, 2.0 * 0.5])
+    f1tenth_policy = create_icra_f1tenth_policy(
         ego_state,
-        "/catkin_ws/src/realm_gc/rgc_control/saved_policies/base/ego_traj.pkl",
-    )
+        "/catkin_ws/src/realm_gc/rgc_control/saved_policies/base/ego_traj.eqx",)
 
     # Test the policies by simulating
     ego_state_trace = []
@@ -124,13 +71,11 @@ if __name__ == "__main__":
 
     for t in jnp.linspace(0, 1.0, steps):
         # Get the observations
-        ego_obs = RALF1tenthObservation(
+        ego_obs = ICRAF1TenthObservation(
             x=ego_state[0],
             y=ego_state[1],
             theta=ego_state[2],
             v=ego_state[3],
-            e = ego_state[4],
-            theta_e = ego_state[5],
             t=t,
         )
        
@@ -203,4 +148,4 @@ if __name__ == "__main__":
     ax.legend()
     ax.set_aspect("equal")
 
-    plt.savefig("src/realm_gc/ral_experiment.png")
+    plt.savefig("src/realm_gc/f1tenth_lqr_experiment.png")
