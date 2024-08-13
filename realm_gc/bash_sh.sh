4a1c958dc30c25b79deb396fb424fbeb6e024655
Kunal Garg <kunalgarg42@gmail.com>
2024-08-13 17:48:14 -0400

    add file name change as bash file, not sure if working

---
+++ realm_gc/bash_sh.sh
@@ -0,0 +1,11 @@
+cd /usr/lib/python3/dist-packages/Cryptodome/Cipher
+rename 's/38/39/' *.so
+
+cd /usr/lib/python3/dist-packages/Cryptodome/Hash
+rename 's/38/39/' *.so
+
+cd /usr/lib/python3/dist-packages/Cryptodome/Util
+rename 's/38/39/' *.so
+
+cd /usr/lib/python3/dist-packages/Cryptodome/Protocol
+rename 's/38/39/' *.so
